a93239fd38f21c442e15c2b435d4fc270025c5e7
AvanMyers <smail1341@gmail.com>
2025-01-14 16:53:57 +0300

    edit

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace RepeatingNumbers
             int arrayMass = 30;
             int checkNumber = 0;
             int checkRepeat = 1;
-            int pinRepeatNumber = 0;
             int pinCheckRepeat = 1;
             int minRandom = 0;
             int maxRandom = 10;
@@ -17,25 +16,36 @@ namespace RepeatingNumbers
 
             int[] array = new int[arrayMass];
 
-            for (int i = 1; i < array.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array[i] = random.Next(minRandom, maxRandom);
-                Console.Write(array[i] + " ");
-
-                if (array[i] != array[0] && array[i] == array[i - 1])
-                {
-                    checkNumber = array[i];
-                    checkRepeat++;
-                }
-                else if (checkRepeat >= pinCheckRepeat)
-                {
-                    pinRepeatNumber = checkNumber;
-                    pinCheckRepeat = checkRepeat;
-                    checkRepeat = 1;
-                }
             }
 
-            Console.Write($" :число {pinRepeatNumber} повторяется {pinCheckRepeat} раза подряд");
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == array[i - 1])
+                {
+                    checkRepeat++;
+
+                    if (checkRepeat >= pinCheckRepeat)
+                    {
+                        checkNumber = array[i];
+                        pinCheckRepeat = checkRepeat;
+                    }
+                }
+                else
+                {
+                    checkRepeat = 1;
+                }
+
+            }
+
+            Console.Write($" :число {checkNumber} повторяется {pinCheckRepeat} раза подряд");
             Console.ReadKey();
         }
     }
